f41338f2f060da3cac5658c5dc925416b3f44ea4
Mike Varga <mikevarga07@gmail.com>
2022-11-09 16:49:46 -0600

    updates to the about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHtml5, faReact, faCss3, faJsSquare, faGitAlt, faPython } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faHtml5,
+  faReact,
+  faCss3,
+  faJsSquare,
+  faGitAlt,
+  faPython,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from "react-loaders"
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -14,43 +22,46 @@ const About = () => {
   // }, [])
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-          letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>INSERT PARAGRAPH ABOUT MYSELF HERE</p>
-        <p>INSERT PARAGRAPH ABOUT MYSELF HERE</p>
-        <p>INSERT PARAGRAPH ABOUT MYSELF HERE</p>
-      </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>INSERT PARAGRAPH ABOUT MYSELF HERE</p>
+          <p>INSERT PARAGRAPH ABOUT MYSELF HERE</p>
+          <p>INSERT PARAGRAPH ABOUT MYSELF HERE</p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faReact} color="#DD0031"/>
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#DD0031"/>
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#DD0031"/>
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faJsSquare} color="#DD0031"/>
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faGitAlt} color="#DD0031"/>
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faPython} color="#DD0031"/>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faPython} color="#EC4" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman"/>
+    </>
   )
 }
 
