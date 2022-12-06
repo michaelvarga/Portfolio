066a0db7d737dad50aa4771bd2795781fd442cf0
Mike Varga <mikevarga07@gmail.com>
2022-12-05 22:18:10 -0600

    Made changes to the landing page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,16 @@
 import { useState, useEffects } from 'react'
 import { Link } from 'react-router-dom'
-import Loader from "react-loaders"
+import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
+import About from "../About"
 import Logo from '../Logo'
 import './index.scss'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const introArray = "Hello! I'm".split('')
-  const nameArray = 'Mike Varga,'.split('')
-  const jobArray = 'software developer'.split('')
+  const introArray = "Hi! I'm Mike".split('')
+  // const nameArray = 'Mike Varga,'.split('')
+  // const jobArray = 'software developer'.split('')
 
   // useEffect(() => {
   //   return setTimeout(() => {
@@ -27,8 +28,9 @@ const Home = () => {
               strArray={introArray}
               idx={5}
             />
-            <br />
-            <AnimatedLetters
+            <span role="img">{String.fromCodePoint(0x1f44b)}</span>
+            {/* <br /> */}
+            {/* <AnimatedLetters
               letterClass={letterClass}
               strArray={nameArray}
               idx={15}
@@ -38,15 +40,28 @@ const Home = () => {
               letterClass={letterClass}
               strArray={jobArray}
               idx={26}
-            />
+            /> */}
           </h1>
+          <p>
+            A passionate Full Stack Software Developer with experience building
+            web and mobile apps with JavaScript / React.js / Node.js / React
+            Native and more.
+          </p>
+          <div>Github, LinkedIn, Email Icons with Links Here</div>
+          {/* Change the contact route */}
           <Link to="/contact" className="flat-button">
-            CONTACT ME
+            Resume
           </Link>
+          <div>What I Do</div>
+          <div>Projects</div>
+          <div>Education</div>
+          <div>Experience</div>
+          <div>Contact Me</div>
         </div>
+        {/* <About /> */}
         <Logo />
       </div>
-      <Loader type="pacman"/>
+      <Loader type="pacman" />
     </>
   )
 }
